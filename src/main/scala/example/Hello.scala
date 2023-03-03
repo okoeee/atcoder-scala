a782dab9d1d6870c920a6dea397a1767b29cfcbb
okoeee <yk27.puxcm@gmail.com>
2023-03-03 17:58:40 +0900

    不要な記述を削除

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -5,33 +5,6 @@ import atcoder.{ABC, ABC212, BeginnerContest, BeginnersSelection}
 object Main {
   def main(args: Array[String]): Unit = {
     println("input value")
-    // BeginnersSelection.practiceA()
-    // BeginnersSelection.ABC086A()
-    // BeginnersSelection.ABC081A
-    // BeginnersSelection.ABC081B
-    // BeginnersSelection.ABC087B
-    // BeginnersSelection.ABC083B
-    // BeginnersSelection.ABC088B
-    // BeginnersSelection.ABC085B
-    // BeginnersSelection.ABC085B_ref
-    // BeginnersSelection.ABC085B_ref_arrange
-    // BeginnersSelection.ABC085C
-    // BeginnersSelection.ABC049C
-    // BeginnersSelection.ABC086C
-    // BeginnerContest.ABC285_A
-    // BeginnerContest.ABC285_B
-    // BeginnerContest.ABC285_C
-    // BeginnerContest.ABC285_D
-    // ABC212.Alloy
-    // ABC212.weakPassword
-    // ABC212.minDifference
-    // ABC212.queryingMultiset
-    // ABC.bitWiseExclusiveOr
-    // ABC.boobyPrize
-    // ABC.howMany
-    // ABC.log2
-    // ABC.oneMoreAabAbaBaa
-    // ABC.signedDifficulty
     ABC.sameName
   }
 }
