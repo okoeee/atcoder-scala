9f0e5c7e500ced3a64119729004dc5c70b2b6af1
okoeee <yk27.puxcm@gmail.com>
2023-04-07 14:17:58 +0900

    validationを追加

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,53 +4,80 @@ import atcoder.{ABC, ABC212, BeginnerContest, BeginnersSelection}
 
 object Main {
 
-  def solution(length: Int, direction: Int, row: Array[Int], col: Array[Int], num: Array[Int]) = {
-    val room = Array.fill(length, length)(0)
+  case class Box(row: Int, col: Int, num: Int)
 
-    val boxes = (0 until row.length).map(i => (row(i), col(i), num(i)))
-    boxes.foreach { case (r, c, n) => room(r)(c) = n }
+  private def validateInputs(length: Int, direction: Int, row: Array[Int], col: Array[Int], num: Array[Int]): Either[String, Unit] = {
+    if (length < 1) Left("lengthの値が不正です")
+    else if (direction < 1 || direction > 4) Left("directionの値が不正です")
+    else if (row.length != col.length || row.length != num.length) Left("row, col, numの配列の数が一致しません")
+    else Right()
+  }
 
-    for {
-      (row, col, num) <- boxes
-      diff <- 0 to num
-      // if row + diff <= length - 1
-    } {
-      val (diffRow, diffCol, preNum) = direction match {
-        case 1 => // 北
-          val diffRow = row + diff
-          val preNum = room(diffRow)(col)
-          (diffRow, col, preNum)
-        case 2 => // 東
-          val diffCol = col - diff
-          val preNum = room(row)(diffCol)
-          (row, diffCol, preNum)
-        case 3 => // 南
-          val diffRow = row - diff
-          val preNum = room(diffRow)(col)
-          (diffRow, col, preNum)
-        case 4 => // 西
-          val diffCol = col + diff
-          val preNum = room(row)(diffCol)
-          (row, diffCol, preNum)
-      }
-
-      if (diff == 0 && preNum == num) room(diffRow)(diffCol) = 0
-      else if (preNum == 0) room(diffRow)(diffCol) = 1
-      else if (preNum < num + 1 - diff && preNum > 0) room(diffRow)(diffCol) = 1 + preNum
-
+  private def isInRange(length: Int, direction: Int, row: Int, col: Int, diff: Int): Boolean = {
+    direction match {
+      case 1 => row + diff <= length - 1
+      case 2 => col - diff >= 0
+      case 3 => row - diff >= 0
+      case 4 => col + diff <= length - 1
     }
+  }
 
-    room.foreach(r => println(r.mkString(" ")))
+  private def sortBoxesByDirection(direction: Int, box: IndexedSeq[Box]): IndexedSeq[Box] = {
+    direction match {
+      case 1 => box.sortWith(_.row < _.row)
+      case 2 => box.sortWith(_.col > _.col)
+      case 3 => box.sortWith(_.row > _.row)
+      case 4 => box.sortWith(_.col < _.col)
+    }
+  }
 
+  private def solution(length: Int, direction: Int, row: Array[Int], col: Array[Int], num: Array[Int]): Unit = {
+    validateInputs(length, direction, row, col, num) match {
+      case Left(message) => println(message)
+      case Right(_) =>
+        val room = Array.fill(length, length)(0)
+        val boxes = (0 until row.length).map(i => Box(row(i), col(i), num(i)))
+        boxes.foreach { case Box(r, c, n) => room(r)(c) = n }
+
+        for {
+          Box(row, col, num) <- sortBoxesByDirection(direction, boxes)
+          diff <- 0 to num
+          if isInRange(length, direction, row, col, diff)
+        } {
+          val (diffRow, diffCol, preNum) = direction match {
+            case 1 =>
+              val diffRow = row + diff
+              val preNum = room(diffRow)(col)
+              (diffRow, col, preNum)
+            case 2 =>
+              val diffCol = col - diff
+              val preNum = room(row)(diffCol)
+              (row, diffCol, preNum)
+            case 3 =>
+              val diffRow = row - diff
+              val preNum = room(diffRow)(col)
+              (diffRow, col, preNum)
+            case 4 =>
+              val diffCol = col + diff
+              val preNum = room(row)(diffCol)
+              (row, diffCol, preNum)
+          }
+          if (diff == 0 && preNum == num) room(diffRow)(diffCol) = 0
+          else if (preNum == 0) room(diffRow)(diffCol) = 1
+          else if (preNum > 0 && preNum < num + 1 - diff && boxes.exists(b => b.row == diffRow && b.col == diffCol)) room(diffRow)(diffCol) = 1 + preNum
+        }
+        room.foreach(row => println(row.mkString(" ")))
+    }
   }
 
   def main(args: Array[String]): Unit = {
     val length = 6
-    val direction = 1
-    val row = Array(0, 2)
-    val col = Array(2, 2)
-    val num = Array(3, 1)
+    val direction = 2
+    val row = Array(2, 2)
+    val col = Array(1, 4)
+    val num = Array(3, 5)
     solution(length, direction, row, col, num)
     // solution(2, 2, Array(0, 1), Array(1, 1), Array(1, 1))
+    // solution(2, 3, Array(0, 0), Array(0, 1), Array(1, 3))
   }
 }
