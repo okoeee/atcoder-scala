86303f62117257f6284d700546683139d216bcab
okoeee <yk27.puxcm@gmail.com>
2023-04-09 15:35:54 +0900

    Future

--- src/main/scala/example/FutureSample.scala
+++ src/main/scala/example/FutureSample.scala
@@ -1,22 +1,41 @@
 package example
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, blocking}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.{BufferedSource, Source}
 import scala.util.{Failure, Success}
 
 object FutureSample {
 
   def exe = {
 
-    val responseFuture = Future(HttpTextClient.get("https://change-x.jp/"))
-
-    responseFuture.onComplete {
-      case Success(body) =>
-        println(body.mkString)
-        body.close
-      case Failure(exception) => println("エラーが発生" + exception.toString)
+    val f: Future[Int] = Future {
+      blocking {
+        Thread.sleep(1000)
+      }
+      24
     }
 
+    f
   }
 
+  // 実行のとき
+//  try {
+//    val futureResult = FutureSample.exe
+//    val result = Await.result(futureResult, Duration.Inf)
+//    println(result)
+//  } catch {
+//    case ex: Throwable => println(ex)
+//  }
+
+  def get(url: String): Future[BufferedSource] = Future(Source.fromURL(url))
+
+  //実行のとき
+//  try {
+//    val futureResult = FutureSample.get("https://scalamatsuri.org/en/")
+//    val result = Await.result(futureResult, Duration.Inf)
+//    println(result.mkString)
+//  } catch {
+//    case exception: Throwable => println(exception)
+//  }
 }
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,6 +3,11 @@ package example
 import atcoder.{ABC, ABC212, BeginnerContest, BeginnersSelection}
 import problems.Solution
 
+import scala.concurrent.{Await, future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
+
 object Main {
 
   def main(args: Array[String]): Unit = {
@@ -18,5 +23,6 @@ object Main {
 
     println("input value")
     ABC.moduloNumber
+
   }
 }
